2913a41fd994f134bb60a043be3768c393b09d4c
Gabriel Palmyro <gabripalmyro13579@gmail.com>
2023-07-18 17:00:46 -0300

    backup notebook

--- src/infra/mqtt/aws-broker/mqtt.service.ts
+++ src/infra/mqtt/aws-broker/mqtt.service.ts
@@ -6,24 +6,21 @@ export class MqttService {
   private client: mqtt.Client;
 
   constructor() {
-    this.client = mqtt.connect(
-      'mqtt://ec2-18-228-3-56.sa-east-1.compute.amazonaws.com',
-      {
-        port: 1883,
-      },
-    );
-
-    this.client.on('error', function (error) {
-      console.log('MQTT client error:', error);
-    });
-
-    this.client.on('offline', function () {
-      console.log('MQTT client offline');
-    });
-
-    this.client.on('close', function () {
-      console.log('MQTT client connection closed');
-    });
+    // this.client = mqtt.connect(
+    //   'mqtt://ec2-15-228-157-24.sa-east-1.compute.amazonaws.com',
+    //   {
+    //     port: 1883,
+    //   },
+    // );
+    // this.client.on('error', function (error) {
+    //   console.log('MQTT client error:', error);
+    // });
+    // this.client.on('offline', function () {
+    //   console.log('MQTT client offline');
+    // });
+    // this.client.on('close', function () {
+    //   console.log('MQTT client connection closed');
+    // });
   }
 
   publish(topic: string, message: string): void {
